91dc34ab884f06066bfb4de81cfac99228fd421a
Fatih Aşlamacı <fatih@debian>
2016-03-06 23:55:14 +0200

    Devam

--- Invoice.go
+++ Invoice.go
@@ -2,10 +2,97 @@ package main
 
 import (
 	"encoding/xml"
+	"time"
 )
+
+type customDate struct {
+	time.Time
+}
+
+type customTime struct {
+	time.Time
+}
+
+func (c *customDate) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	const shortForm = "2006-01-02" // yyyymmdd date format
+	var v string
+	d.DecodeElement(&v, &start)
+	parse, err := time.Parse(shortForm, v)
+	if err != nil {
+		return err
+	}
+	*c = customDate{parse}
+	return nil
+}
+
+func (c *customTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	const shortForm = "15:04:05" // yyyymmdd date format
+	var v string
+	d.DecodeElement(&v, &start)
+	parse, err := time.Parse(shortForm, v)
+	if err != nil {
+		return err
+	}
+	*c = customTime{parse}
+	return nil
+}
 
 type Invoice struct {
 	XMLName         xml.Name `xml:"Invoice"`
 	UBLVersionID    string
 	CustomizationID string
+
+	ProfileID            string
+	ID                   IdType
+	CopyIndicator        string
+	UUID                 string
+	IssueDate            customDate
+	IssueTime            customTime
+	InvoiceTypeCode      string
+	DocumentCurrencyCode string
+	LineCountNumeric     int
+	Signature            SignatureType
+}
+
+type IdentifierType struct {
+	SchemeID         string `xml:"schemeID,attr,omitempty"`
+	SchemeName       string `xml:"schemeName,attr,omitempty"`
+	SchemeAgencyID   string `xml:"schemeAgencyID,attr,omitempty"`
+	SchemeAgencyName string `xml:"schemeAgencyName,attr,omitempty"`
+	SchemeVersionID  string `xml:"schemeVersionID,attr,omitempty"`
+	SchemeDataURI    string `xml:"schemeDataURI,attr,omitempty"`
+	SchemeURI        string `xml:"schemeURI,attr,omitempty"`
+	Value            string `xml:",chardata"`
+}
+
+type SignatureType struct {
+	ID IdType
+
+	SignatoryParty PartyType
+
+	//private AttachmentType digitalSignatureAttachmentField;
+}
+
+type PartyType struct {
+	WebsiteURI string
+
+	//private EndpointIDType endpointIDField;
+	//private IndustryClassificationCodeType industryClassificationCodeField;
+	//PartyIdentification []PartyIdentificationType
+	//private PartyNameType partyNameField;
+	//private AddressType postalAddressField;
+	//private PartyTaxSchemeType partyTaxSchemeField;
+	//private PartyLegalEntityType[] partyLegalEntityField;
+	//private ContactType contactField;
+	//private PersonType personField;
+	//private PartyType agentPartyField;
+
+}
+
+type IdentifierType1 struct {
+	IdentifierType
+}
+
+type IdType struct {
+	IdentifierType1
 }
--- faturago_test.go
+++ faturago_test.go
@@ -8,7 +8,7 @@ import (
 	//"os"
 )
 
-func stringKarsilastir(t *testing.T, alanAdi, beklenen, gerceklesen string) {
+func stringKarsilastir(t *testing.T, alanAdi, gerceklesen, beklenen string) {
 	if gerceklesen != beklenen {
 		t.Errorf("Hata : '%s' alanı '%s' beklenirken '%s' geldi.", alanAdi, beklenen, gerceklesen)
 	}
@@ -24,7 +24,10 @@ func TestInvoiceDeserialize(t *testing.T) {
 		t.Errorf("XML dosya hatalı: %v", err)
 	}
 
-	stringKarsilastir(t, "invoice.UBLVersionID", "2.1", invoice.UBLVersionID)
-	stringKarsilastir(t, "invoice.CustomizationID", "TR1.2", invoice.CustomizationID)
+	stringKarsilastir(t, "invoice.UBLVersionID", invoice.UBLVersionID, "2.1")
+	stringKarsilastir(t, "invoice.CustomizationID", invoice.CustomizationID, "TR1.2")
+	stringKarsilastir(t, "invoice.IssueDate", invoice.IssueDate.String(), "2009-01-05 00:00:00 +0000 UTC")
+	stringKarsilastir(t, "invoice.IssueTime", invoice.IssueTime.String(), "0000-01-01 14:42:00 +0000 UTC")
+	stringKarsilastir(t, "invoice.ID.Value", invoice.ID.Value, "GIB2009000000011")
 
 }
